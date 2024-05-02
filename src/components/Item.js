66b71b6961f3b26ef0fc03c725ba8441f154eb84
amososwom <amososwom162@gmail.com>
2024-05-02 08:16:59 +0300

    completed state-events-lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false)
+  const liItem = inCart ?  "in-cart" : "";
+  const btnItem = inCart ?  "Remove From Cart" : "Add to Cart";
+
   return (
-    <li className="">
+    <li className={liItem}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={() => setInCart(!inCart)} >{btnItem}</button>
     </li>
   );
 }
